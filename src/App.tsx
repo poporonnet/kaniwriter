30dabb542fc62f8cfa8f014ec2a817dc797f6a28
tufusa <tufusa74@gmail.com>
2025-02-25 00:37:16 +0900

    feat: Alertを通知表示に変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "routes/router";
 import "../i18n/i18n";
+import { NotificationProvider } from "components/NotificationProvider";
 
-export const App = () => {
-  return <RouterProvider router={router} />;
-};
+export const App = () => (
+  <NotificationProvider>
+    <RouterProvider router={router} />
+  </NotificationProvider>
+);

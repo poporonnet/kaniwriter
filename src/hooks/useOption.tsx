20ea6c3e2c03565a49af74a3cb9b169ff4294d21
tufusa <tufusa74@gmail.com>
2025-04-12 00:32:28 +0900

    refactor: controlしたOptionListコンポーネントを提供するuseOptionフックを追加

---
+++ src/hooks/useOption.tsx
@@ -0,0 +1,31 @@
+import { ComponentType, useState } from "react";
+import { useStoreState } from "./useStoreState";
+import { OptionList } from "components/OptionList";
+
+type UseOptionList = [
+  list: ComponentType,
+  { autoScroll: boolean; autoConnect: boolean; autoVerify: boolean }
+];
+
+export const useOption = (): UseOptionList => {
+  const [autoScroll, setAutoScroll] = useState(true);
+  const [autoConnect, setAutoConnect] = useStoreState("autoConnect", false);
+  const [autoVerify, setAutoVerify] = useStoreState("autoVerify", false);
+
+  return [
+    () => (
+      <OptionList
+        autoScroll={autoScroll}
+        setAutoScroll={setAutoScroll}
+        autoConnect={autoConnect}
+        setAutoConnect={(value) => {
+          setAutoConnect(value);
+          if (value) window.location.reload();
+        }}
+        autoVerify={autoVerify}
+        setAutoVerify={setAutoVerify}
+      />
+    ),
+    { autoScroll, autoConnect, autoVerify },
+  ];
+};

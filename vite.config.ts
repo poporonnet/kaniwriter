b064f3efe7cdf69c81d70322b08a03b160b96e5f
Miharup <kijiharu3112@outlook.jp>
2025-05-10 19:45:55 +0900

    chore:Sentryの設定修正できたかも

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, loadEnv } from "vite";
+import {sentryVitePlugin} from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,7 +11,14 @@ export default defineConfig(({ mode }) => {
     build: {
       sourcemap: true,
     },
-    plugins: [reactRouter()],
+    plugins: [
+      reactRouter(),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "poporon-network",
+        project: "kaniwriter"
+      })
+    ],
     base: process.env.VITE_BASE_URL,
     resolve: {
       alias: {

190b7c9fbbe5fb0f9ae41f34fdb7619ffa722e68
tufusa <me@tufusa.net>
2025-12-13 23:20:10 +0900

    chore: 型名を Device に修正 既存の Target 型との混乱を避けるため

--- src/libs/mrbwrite/adapter.ts
+++ src/libs/mrbwrite/adapter.ts
@@ -1,14 +1,14 @@
 import { Result } from "libs/result";
 import { Profile } from "./profile";
 
-export interface MrbwriteAdapter<Target> {
+export interface MrbwriteAdapter<Device> {
   getProfile(): Profile | undefined;
   setProfile(profile: Profile | undefined): void;
 
   isConnected(): boolean;
 
   request(option?: {
-    customRequest?: () => Promise<Target>;
+    customRequest?: () => Promise<Device>;
   }): Promise<Result<void, Error>>;
   open(): Promise<Result<void, Error>>;
   close(): Promise<Result<void, Error>>;

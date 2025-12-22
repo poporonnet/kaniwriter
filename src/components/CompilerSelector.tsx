488cf29d83d777613b777676d926d6c3aa9bedee
tufusa <me@tufusa.net>
2025-12-23 00:21:03 +0900

    feat: `CompilerSelector` コンポーネントを Mantine に移行

--- src/components/CompilerSelector.tsx
+++ src/components/CompilerSelector.tsx
@@ -1,14 +1,13 @@
-import { Option, Select } from "@mui/joy";
-import { SxProps } from "@mui/joy/styles/types";
+import { Select, Text, ThemeIcon } from "@mantine/core";
 import { Version } from "hooks/useVersions";
 import { useTranslation } from "react-i18next";
+import { MdUnfoldMore as UnfoldMoreIcon } from "react-icons/md";
 
 type Props = {
   versions: Version[];
   version: string;
   disabled?: boolean;
   onChange?: (version: Version) => void;
-  sx?: SxProps;
 };
 
 export const CompilerSelector = ({
@@ -16,31 +15,76 @@ export const CompilerSelector = ({
   version,
   disabled,
   onChange,
-  sx,
 }: Props) => {
   const [t] = useTranslation();
 
   return (
     <Select
-      variant="soft"
       placeholder={t("コンパイラを選択")}
-      onChange={(_, value) => {
-        if (onChange && typeof value === "string") {
-          onChange(value);
+      value={version}
+      onChange={(value) => {
+        if (typeof value === "string") {
+          onChange?.(value);
         }
       }}
-      value={version}
-      sx={{
-        pl: "1rem",
-        ...sx,
-      }}
       disabled={disabled}
-    >
-      {versions.map((version) => (
-        <Option value={version} key={version}>
-          mrbc {version}
-        </Option>
-      ))}
-    </Select>
+      withCheckIcon
+      withScrollArea={false}
+      checkIconPosition="right"
+      data={versions.map((version) => ({
+        value: version,
+        label: `mrbc ${version}`,
+      }))}
+      variant="filled"
+      rightSection={
+        <ThemeIcon
+          size="fit-content"
+          variant="transparent"
+          c={disabled ? "neutral.4" : "neutral.5"}
+          bd={0}
+        >
+          <UnfoldMoreIcon size="1.25rem" />
+        </ThemeIcon>
+      }
+      renderOption={(item) => (
+        <Text
+          px="0.75rem"
+          py="0.375rem"
+          w="100%"
+          c="neutral.7"
+          fz="md"
+          bg={item.checked ? "neutral.3" : "transparent"}
+        >
+          {item.option.label}
+        </Text>
+      )}
+      comboboxProps={{ offset: 3, shadow: "md" }}
+      radius="6px"
+      styles={(theme) => ({
+        input: {
+          fontSize: "1rem",
+          paddingLeft: "1rem",
+          border: 0,
+          color: theme.colors.neutral[7],
+          background: disabled
+            ? theme.colors.neutral[0]
+            : theme.colors.neutral[1],
+          boxShadow: "0px 1px 2px 0px rgba(21 21 21 / 0.08)",
+        },
+        section: {
+          width: "fit-content",
+          marginRight: "8px",
+        },
+        dropdown: {
+          padding: "6px 0",
+          border: 0,
+          background: theme.colors.neutral[1],
+        },
+        option: {
+          padding: 0,
+          borderRadius: 0,
+        },
+      })}
+    />
   );
 };

1c1a649d0182a70b6bf187f4cc844c9e764d6d42
tufusa <me@tufusa.net>
2025-12-27 01:22:24 +0900

    chore: ビルドモードとベースURLを整理 (#1001)
    * chore: VITE_BASE_URL 環境変数を削除
    
    * chore: 常に埋め込みデプロイを想定する
    
    * chore: ceres-production, ceres-staging モードを削除
    
    * chore: ceres 用ビルドコマンドを build:ceres に統合
    
    * docs: ドキュメントを更新
    常に相対パスで見るようになったので

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,7 +7,6 @@ interface ViteTypeOptions {
 interface ImportMetaEnv {
   readonly NPM_PACKAGE_VERSION: string;
   readonly VITE_COMPILER_URL: string;
-  readonly VITE_BASE_URL: string;
   readonly VITE_WRITER_REPOSITORY_PATH: string;
   readonly VITE_REFERENCE_URL: string;
   readonly VITE_COMPILER_VERSION_FALLBACK: string;

23027aef647ff13e111490b1e34123fce80ce022
Miharup <74781355+midokuroku@users.noreply.github.com>
2026-02-08 23:07:04 +0900

    chore: format

--- src/utils/sentryInitialize.ts
+++ src/utils/sentryInitialize.ts
@@ -2,9 +2,6 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  ignoreErrors: [
-    "Already write mode.",
-    "No port.",
-  ],
+  ignoreErrors: ["Already write mode.", "No port."],
   sendDefaultPii: true,
 });

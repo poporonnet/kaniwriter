8299e908da70149afb7021f8952c7bde560a6348
Teselapfer <schwarz44571@gmail.com>
2026-05-28 23:59:20 +0900

    chore: react-router.config.tsの削除

--- react-router.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-  appDirectory: "src",
-  buildDirectory: "dist",
-  ssr: false,
-  basename: process.env.VITE_BASE_URL,
-} satisfies Config;

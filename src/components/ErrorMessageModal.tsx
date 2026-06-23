237c410afc973476d0315c9fb8a1246c0a272f03
kiharu <74781355+kiharu3112@users.noreply.github.com>
2026-06-24 08:38:52 +0900

    feat(develop/replace-ui): ErrorMessageModal を Mantine に移行 (#1280)
    * feat: ErrorMessageModal を Mantine に移行
    
    * chore: overlay
    
    * format
    
    * スタイル調整
    
    * format
    
    * スタイル修正
    
    * スタイル修正
    
    * format

--- src/components/ErrorMessageModal.tsx
+++ src/components/ErrorMessageModal.tsx
@@ -1,4 +1,4 @@
-import { Sheet } from "@mui/joy";
+import { Overlay, Paper } from "@mantine/core";
 
 export const ErrorDetailModal = ({
   error,
@@ -8,48 +8,34 @@ export const ErrorDetailModal = ({
   error: string;
   isOpen: boolean;
   setIsOpen: (value: boolean) => void;
-}) => (
-  <>
-    {isOpen && (
-      <>
-        <Sheet
-          onClick={() => setIsOpen(false)}
-          sx={{
-            top: 0,
-            left: 0,
-            width: "min(100dvw, 100vw)",
-            height: "min(100dvh, 100vh)",
-            position: "fixed",
-            background: "rgba(0, 0, 0, 0.5)",
-            zIndex: 3,
-          }}
-        />
-        <Sheet
-          variant="outlined"
-          sx={{
-            textAlign: "left",
-            left: 0,
-            position: "absolute",
-            zIndex: "4",
-            width: "60rem",
-            minWidth: "30rem",
-            maxWidth: "calc(100vw - 10rem)",
-            maxHeight: "30rem",
-            overflow: "auto",
-            top: "10.5rem",
-            p: "1rem",
-            borderRadius: "0.3rem",
-            borderColor: "#FFBBBB",
-            display: "flex",
-            flexDirection: "column",
-            boxShadow: "0px 3px 10px gray",
-          }}
-        >
-          {error.split("\n").map((t) => (
-            <code>{t}</code>
-          ))}
-        </Sheet>
-      </>
-    )}
-  </>
-);
+}) =>
+  isOpen && (
+    <>
+      <Overlay fixed backgroundOpacity={0.5} onClick={() => setIsOpen(false)} />
+      <Paper
+        withBorder
+        pos="absolute"
+        top="100%"
+        left="0"
+        w="60rem"
+        miw="30rem"
+        maw="calc(100vw - 10rem)"
+        mah="30rem"
+        p="1rem"
+        radius="0.3rem"
+        ta="left"
+        style={{
+          zIndex: "var(--mantine-z-index-modal)",
+          overflow: "auto",
+          borderColor: "#FFBBBB",
+          display: "flex",
+          flexDirection: "column",
+          boxShadow: "0px 3px 10px gray",
+        }}
+      >
+        {error.split("\n").map((t, i) => (
+          <code key={i}>{t}</code>
+        ))}
+      </Paper>
+    </>
+  );

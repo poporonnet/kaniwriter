ba13104885fc16366db299ebd88391bb94933aed
Miharup <74781355+kiharu3112@users.noreply.github.com>
2026-07-02 22:34:01 +0900

    chore: いい感じになった

--- src/components/UnsupportedBrowserModal.tsx
+++ src/components/UnsupportedBrowserModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalClose, Sheet, Typography } from "@mui/joy";
+import { CloseButton, Modal, Text, Title } from "@mantine/core";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -11,30 +11,40 @@ export const UnsupportedBrowserModal = ({
 }: UnsupportedBrowserModalProps) => {
   const [open, setOpen] = useState(defaultOpen);
   const [t] = useTranslation();
+
   return (
     <Modal
-      open={open}
+      opened={open}
       onClose={() => setOpen(false)}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "90%",
-        minWidth: "500px",
-        m: "auto",
-        maxWidth: "60rem",
+      withCloseButton={false}
+      centered
+      size="50rem"
+      radius="0.5rem"
+      padding={0}
+      styles={{
+        content: {
+          width: "80%",
+          minWidth: "500px",
+          border: "1px solid var(--mantine-color-neutral-3)",
+        },
       }}
     >
-      <Sheet variant="outlined" sx={{ borderRadius: "0.5rem" }}>
-        <Typography level="h4" m={1} alignItems="center">
-          {t("このブラウザはサポートされていません")}
-        </Typography>
-        <ModalClose variant="plain" sx={{ m: 0.5 }} />
+      <Title order={4} mt="0.5rem" m="0.5rem" style={{fontWeight: "bold"}}>
+        {t("このブラウザはサポートされていません")}
+      </Title>
+ 
+      <CloseButton
+        variant="transparent"
+        onClick={() => setOpen(false)}
+        pos="absolute"
+        top="0.75rem"
+        right="0.75rem"
+        m="0.25rem"
+      />
 
-        <Typography fontSize="sm" m="0.5rem" whiteSpace="pre-line">
-          {t("対応するブラウザを使ってください")}
-        </Typography>
-      </Sheet>
+      <Text fz="0.8rem" m="0.5rem" style={{ whiteSpace: "pre-line" }}>
+        {t("対応するブラウザを使ってください")}
+      </Text>
     </Modal>
   );
 };
